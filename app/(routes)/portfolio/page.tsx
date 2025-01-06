99e8595697d4f38f7d3266981851ac55d9f19aa7
VictorDev1986 <VictorDev1986@gmail.com>
2025-01-06 04:21:44 -0500

    Creando la pagina de contacto

--- app/(routes)/portfolio/page.tsx
+++ app/(routes)/portfolio/page.tsx
@@ -1,5 +1,4 @@
-import Image from "next/image";
-import Link from "next/link";
+"use client"
 import { CoverParticles } from "@/components/cover-particles";
 
 import { dataPortfolio } from "@/data";
--- components/contact-page.tsx
+++ components/contact-page.tsx
@@ -2,25 +2,27 @@ import React from 'react';
 
 const ContactPage = () => {
     return (
-        <div className="flex flex-col md:flex-row h-min py-8">
-            {/* Left side with a message */}
-            <div className="w-full md:w-1/2 bg-black/30 flex items-center justify-center text-white p-6">
-                <h1 className="text-2xl md:text-4xl font-bold text-center px-4">
-                    Estoy buscando empleo. ¡Contáctame para colaborar juntos!
+        <div className="flex flex-col md:flex-row h-min py-14 gap-4">
+            
+            <div className="w-full md:w-1/2 bg-black/20 rounded-lg flex items-center justify-center p-6">
+                <h1 className="text-xl md:text-3xl font-bold text-center px-4">
+                    "En busca de nuevas oportunidades. ¡Hablemos y trabajemos juntos!"
                 </h1>
             </div>
 
-            <div className="w-full md:w-1/2 bg-black/30 flex flex-col items-center justify-center p-8">
-                <h2 className="text-xl md:text-2xl font-semibold mb-4">Escribeme</h2>
-                <p className="mb-4 text-while text-center">victordev1986@gmail.com</p>
+            <div className="w-full md:w-1/2 bg-black/20 rounded-lg flex flex-col items-center justify-center p-8">
+                <p className="text-xl mb-4 text-white text-center">
+                    <a href="mailto:victordev1986@gmail.com">victordev1986@gmail.com</a>
+                </p>
                 <a
-                    href="/path/to/your-cv.pdf" // Reemplaza con la URL de tu CV
+                    href="/public/CV.pdf" // Reemplaza con la URL de tu CV
                     download
-                    className="bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-600 transition duration-300 text-center"
+                    className="p-2 transition duration-150 rounded-lg border-2 text-white border-green-500 hover:shadow-xl hover:shadow-green-500"
                 >
                     Descargar CV
                 </a>
             </div>
+            
         </div>
     );
 };
